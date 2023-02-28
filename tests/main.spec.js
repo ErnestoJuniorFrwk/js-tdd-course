df62058c4cb6e2cac2bf551c2d94cf835f87510c
ErnestoJuniorFrwk <ernestojunior@frwk.com.br>
2023-02-28 04:15:46 -0300

    chore(eslint): fix configuration

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,9 +1,9 @@
-import { expect } from "chai";
-import { search } from "../src/main";
+import { expect } from 'chai';
+import { search } from '../src/main';
 // searchAlbums, searchArtists, searchTracks, searchPlaylists
-describe("Wrapper Spotify", () => {
-  describe("smoke tests", () => {
-    it("Should exist the search method", () => {
+describe('Wrapper Spotify', () => {
+  describe('smoke tests', () => {
+    it('Should exist the search method', () => {
       // eslint-disable-next-line no-unused-expressions
       expect(search).to.exist;
     });
